8131096f2f49ca11ab42bdef52fb1ba697930fb3
Lukasfenkart <90911645+Lukasfenkart@users.noreply.github.com>
2022-09-23 13:49:47 +0200

    class added

--- Genericlist/GenericClass.cs
+++ Genericlist/GenericClass.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Genericlist
 {
-    internal class Class1
+    internal class GenericClass
     {
     }
 }
--- Genericlist/Program.cs
+++ Genericlist/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+namespace GenerischeKlassen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var itemStore = new ItemStore<int, double, string>();
+            itemStore.Value1 = 5;
+            itemStore.Value2 = 4.5;
+            itemStore.Value3 = "2";
+
+            var summe = 6 + itemStore.Value1;
+
+            Console.WriteLine(summe);
+            Console.WriteLine(itemStore.Value2);
+
+        }
+    }
+
+    class ItemStore<TValueType1, TValueType2, TValueType3>
+    {
+        public TValueType1 Value1 { get; set; }
+        public TValueType2 Value2 { get; set; }
+        public TValueType3 Value3 { get; set; }
+    }
+}
