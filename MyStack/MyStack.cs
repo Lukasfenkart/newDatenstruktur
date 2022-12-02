c28bdf04b75308d20479abab4ac393c286d453af
Lukasfenkart <90911645+Lukasfenkart@users.noreply.github.com>
2022-12-02 12:12:48 +0100

    final changes

---
+++ MyStack/MyStack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace MyStack
+{
+    public class MyStack
+    {
+        LinkedList.LinkedList internalList = new LinkedList.LinkedList();
+
+        public Node Push(int newNode)
+        {
+            var nodeToAdd = new Node(newNode, null);
+            internalList.insertlast(newNode);
+            return nodeToAdd;
+        }
+
+        public Node Pop()
+        {
+            var retval = internalList.GetLast();
+            internalList.deletelast();
+            return retval;
+        }
+
+        public override string ToString()
+        {
+            return internalList.ToString();
+        }
+
+    }
+}
